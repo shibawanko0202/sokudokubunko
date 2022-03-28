c25500ddc25a074e950edaa16b7de439eb17f49b
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2022-03-28 18:20:53 +0900

    永訣の朝の注釈の追加

--- js/main.js
+++ js/main.js
@@ -29,12 +29,43 @@ let bookmark = 0;
 let timer;
 let is_play = false;
 
-//目次の表示
-for(let i = 0;i < content.length;i++){
-  content_title[i].textContent = books[i].title;
-  content_chapter[i].textContent = books[i].chapter;
-  content_author[i].textContent = books[i].author;
-  seconds[i].textContent = to_kansuzi(Math.ceil((books[i].text.length / 5) * 1.3));
+//表紙の表示
+function render_cover(num){
+  current_title = books[num];
+  bookmark = 0;
+  cover_author.textContent = books[num].author;
+  cover_title.textContent = books[num].title;
+  flip_page(books[num]);
+  textbord.textContent = "ここに注目して下さい";
+};
+
+//文字送り
+function flip_page(title){
+  textbord.textContent = title.text[bookmark];
+  for(let i = 0;i < after.length;i++){
+    after[i].textContent = title.text[bookmark - (i + 1)];
+  };
+  if(bookmark == title.text.length -1){
+    stopped();
+    start.classList.add("disabled");
+  };
+  commentary(title);
+  bookmark++;
+};
+
+//停止
+function stopped(){
+  start.classList.remove("disabled");
+  stop.classList.add("disabled");
+  clearInterval(timer);
+  is_play = false;
+};
+
+//表紙の開閉
+function turn_cover(){
+  cover.classList.toggle("open");
+  cover_reverse.classList.toggle("open");
+  stopped();
 };
 
 //漢数字に変換
@@ -45,27 +76,6 @@ function to_kansuzi(num){
     str += `${txt[String(num).charAt(i)]}`;
   };
   return str;
-};
-
-//文字送り
-function flip_page(title){
-  textbord.textContent = title.text[bookmark];
-  for(let i = 0;i < after.length;i++){
-    after[i].textContent = title.text[bookmark - (i + 1)];
-  };
-  if(bookmark == title.text.length -1){
-    clearInterval(timer);
-    stop.classList.add("disabled");
-  };
-  commentary(title);
-  bookmark++;
-};
-
-//表紙の開閉
-function turn_cover(){
-  cover.classList.toggle("open");
-  cover_reverse.classList.toggle("open");
-  stopped();
 };
 
 //注釈を表示
@@ -103,6 +113,9 @@ window.addEventListener('resize',resize);
 //ロード終了時にアニメーション
 window.onload = setTimeout(turn_cover, 750);
 
+//最初は「こころ」を表示しておく
+render_cover(0);
+
 //開始ボタン
 start.addEventListener("click",()=>{
   start.classList.add("disabled");
@@ -116,17 +129,10 @@ start.addEventListener("click",()=>{
 //停止ボタン
 stop.addEventListener("click",stopped);
 
-//停止
-function stopped(){
-  start.classList.remove("disabled");
-  stop.classList.add("disabled");
-  clearInterval(timer);
-  is_play = false;
-};
-
 //巻き戻し
-for(let i = 0;i <= 7;i++){
+for(let i = 0;i < after.length;i++){
   after[i].addEventListener("click",()=>{
+    start.classList.remove("disabled");
     if(is_play){
       return;
     };
@@ -140,6 +146,14 @@ signbord.addEventListener("click",turn_cover);
 turn.addEventListener("click",turn_cover);
 cover_reverse.addEventListener("click",turn_cover);
 
+//目次の表示
+for(let i = 0;i < content.length;i++){
+  content_title[i].textContent = books[i].title;
+  content_chapter[i].textContent = books[i].chapter;
+  content_author[i].textContent = books[i].author;
+  seconds[i].textContent = to_kansuzi(Math.ceil((books[i].text.length / 5) * 1.3));
+};
+
 //目次からタイトルを選択
 for(let i = 0;i < content.length;i++){
   content[i].addEventListener("click",()=>{
@@ -147,11 +161,6 @@ for(let i = 0;i < content.length;i++){
     if(current_title === books[i]){
       return;
     };
-    current_title = books[i];
-    bookmark = 0;
-    cover_author.textContent = books[i].author;
-    cover_title.textContent = books[i].title;
-    flip_page(books[i]);
-    textbord.textContent = "ここに注目して下さい";
+    render_cover(i);
   });
 };
